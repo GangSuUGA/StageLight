85b02d8868f3da3df322a12f3d98ccde09830f97
Gang Su <88390140+GangSuUGA@users.noreply.github.com>
2022-02-17 19:50:39 -0500

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -23,8 +23,8 @@ class Visualization:
         plt.ylim(min_val - 0.05 * abs(min_val), max_val + 0.05 * abs(max_val))
         fig = plt.gcf()
         fig.set_size_inches(20, 11.25)
+        fig.show()
         fig.savefig(os.path.join(self._path, 'plot_'+filename+'.png'), dpi=self._dpi)
-        plt.close("all")
 
         with open(os.path.join(self._path, 'plot_'+filename + '_data.txt'), "w") as file:
             for value in data:
